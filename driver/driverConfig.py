05d0f220b9f4a90dc64848a01090b3a4ad467c1c
luana_acostaARB <luana.acosta@arbusta.net>
2023-04-23 15:46:47 -0300

    Uno

---
+++ driver/driverConfig.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+
+#en este archivo se define el driver general y cómo se ejecutaran las pruebas
+
+class Driver:
+    def __init__(self):
+        self.driver = None
+
+    def aplicacion(self):
+        url = 'https://www.saucedemo.com/'
+        return url
+
+    def set_driver(self):
+        option = Options()
+        #option.add_argument('--headless')
+        option.add_argument('start-maximized')
+        #self.driver = webdriver.Chrome(options=option, service=Service(GeckoDriverManager().install()))
+        self.driver = webdriver.Chrome(options=option, service=Service(ChromeDriverManager().install()))
+        self.driver.implicitly_wait(30)
+        web = self.aplicacion()
+        self.driver.get(web)
+
+        return self.driver
